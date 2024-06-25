5a6a6a081a04bcef9f16e09c5212216c481ef80f
Markomrnkvc <marko.marinkovic@study.hs-duesseldorf.de>
2024-06-25 13:27:07 +0200

    minor updates in statObj.py

--- statObj.py
+++ statObj.py
@@ -11,19 +11,21 @@ class staticFilter():
     def __init__(self, shape):  
         self.shape = shape
         self.measurement_variance = np.eye(1) * 0.04
-        self.state_variance = np.eye(1) * 100 #random value
+        self.state_variance = np.eye(1) * 343 #random value
         self.state_estimate = np.zeros(1)
         
 
     def reset(self, measurement):    
-        return measurement[:2]
+        self.state_estimate = measurement[:2]
+        return self.state_estimate
     
     def update(self, dt, measurement):  
-        num = len(measurement)
-        for i in range(num):
-            t = self.measurement_variance/(self.measurement_variance + self.state_variance)
-            self.state_estimate = t*self.state_estimate + (1-t) * measurement
-            self.state_variance = t**2 * self.state_variance + (1-t)**2 * self.measurement_variance
+        #num = len(measurement)
+        #for i in range(num):
+        t = self.measurement_variance/(self.measurement_variance + self.state_variance)
+        self.state_estimate = t*self.state_estimate + (1-t) * measurement
+        self.state_variance = t**2 * self.state_variance + (1-t)**2 * self.measurement_variance
+        #print(self.state_estimate)
             
             
-        return np.array([self.state_estimate[0][0],self.state_estimate[1][1]]) #otherwise it return an 2x2 Array
+        return np.array([self.state_estimate[0][0],self.state_estimate[1][1]]) #otherwise it returns an 2x2 Array
